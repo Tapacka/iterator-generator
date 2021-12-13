71eab7157fe367936c1520743c87580b8e3e0e0c
Tapacka <88617836+Tapacka@users.noreply.github.com>
2021-12-13 21:36:50 +0300

    Update iterator.py
    добавил sum()

--- iterator.py
+++ iterator.py
@@ -1,31 +1,36 @@
 nested_list = [
 	['a', 'b', 'c'],
 	['d', 'e', 'f'],
-	[1, None, 3], [['q', 'w', 'e'], ['!' , '@', '#'], ['u' , 'i', 'o']]]
+	[1, None, 3], ['q', 'w', 'e'], ['!' , '@', '#'], ['u' , 'i', 'o']]
 
 class FlatIterator:
   def __init__(self, list_):
-    self.list_ = list_
+    self.list_ = sum(list_,[])
 
   def __iter__(self):
+    
     self.count = -1
     return self
     
   def __next__(self):
-    self.count += 1
-    
+    self.l=[]
+    self.count += 1 
     if self.count == len(self.list_):
-      raise StopIteration
-    self.newlist = self.list_[self.count]
+      raise StopIteration    
     
-    l = []
-    if isinstance(self.newlist, list):      
-      for i in FlatIterator(self.newlist):        
-        l.extend(i)              
-    else:
-      l.append(self.newlist)
+    self.newlist = self.list_[self.count]
      
-    return(l)
+    
+    
+    if isinstance(self.newlist, list):      
+      for i in FlatIterator(self.newlist):
+        return i
+                   
+    else:
+      return self.newlist
+    
+    
+    
     
 for item in FlatIterator(nested_list):
-  print(*item, sep='\n')
+  print(item)
